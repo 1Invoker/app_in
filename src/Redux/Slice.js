65a619b00d5549296f01b23467c8d572ec9177db
1Invoker <sovun000@gmail.com>
2024-04-17 17:50:02 +0300

    wip

--- src/Redux/Slice.js
+++ src/Redux/Slice.js
@@ -1,21 +1,21 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 const inicialState = {
-    count: 0,
-    todos: [],
+  count: 0,
+  todos: [],
 };
 export const todoSlice = createSlice({
-    name: "todo",
-    inicialState,
-    redusers: {
-        addTodo: (state, action) => {
-            state.todos.push(action.payload);
-        },
-        removeTodo: (state,action) => {
-            state.todos.splice(action.payload, 1);
-        },
+  name: 'todo',
+  inicialState,
+  redusers: {
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
     },
+    removeTodo: (state, action) => {
+      state.todos.splice(action.payload, 1);
+    },
+  },
 });
 
-export const {addTodo, removeTodo} = todoSlice.actions;
+export const { addTodo, removeTodo } = todoSlice.actions;
 
-export default todoSlice.reducer; 
+export default todoSlice.reducer;
