2195307617992a4e6089daca8ef11d2c64bb8799
Даниил Ключников <d.klyuchnikov@gbubr.local>
2024-06-19 16:14:00 +0300

    add, search

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import ToDo from '../src/Components/ToDo/ToDo';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
       <ToDo />
     </div>
   );
--- src/Components/ToDo/ToDo.js
+++ src/Components/ToDo/ToDo.js
@@ -4,6 +4,7 @@ import './ToDo.css';
 const ToDo = () => {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [searchTitle, setSearchTitle] = useState('');
 
   const handleInputChange = e => {
     setInputValue(e.target.value);
@@ -14,6 +15,43 @@ const ToDo = () => {
       setInputValue('');
     }
   };
-  return <></>;
+  const deleteTodo = indexToDel => {
+    const updateToDos = todos.filter((todo, index) => index !== indexToDel);
+    setTodos(updateToDos);
+  };
+
+  const updateSearch = todos.filter(todo =>
+    todo.toLowerCase().includes(searchTitle.toLowerCase()),
+  );
+  return (
+    <div className="todo-container">
+      <h1>Todo List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Add a new todo..."
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <div className="search-container">
+        <input
+          type="text"
+          value={searchTitle}
+          onChange={e => setSearchTitle(e.target.value)}
+          placeholder="Search by title..."
+        />
+      </div>
+      <ul>
+        {updateSearch.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default ToDo;
